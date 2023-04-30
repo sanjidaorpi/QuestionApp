e949649db5d183cab50be2a62a3265406f721d97
Sanjida Orpi <so2189@nyu.edu>
2023-04-30 18:06:01 -0400

    Initial Commit

---
+++ QuestionApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  QuestionApp
+//
+//  Created by Sanjida Orpi on 4/30/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ QuestionApp/QuestionAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestionAppApp.swift
+//  QuestionApp
+//
+//  Created by Sanjida Orpi on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct QuestionAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
